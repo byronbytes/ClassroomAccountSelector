6f88a9554a5b4df43f0a2c799a57009f6b3f2399
Byron <53088136+byronbytes@users.noreply.github.com>
2021-04-22 09:35:10 -0400

    v1.7 - Spelling Fixes

--- googleclassroom_selector.py
+++ googleclassroom_selector.py
@@ -2,28 +2,24 @@ import webbrowser
 import os
 
 
-print('This only works if you have multiple google accounts.')
-os.title('Google Classroom Selector')
+print('This works if you have multiple Google accounts.')
+os.title('Google Classroom Account Selector')
 print('')
 os.system('pause')
-print('[1] First Google Account Signed In / Default User')
+print('[1] First account signed in')
 print('')
-print('[2] Second Google Account Signed In')
+print('[2] Second account signed in')
 print('')
-print('[3] Third Google Account Signed In')
+print('[3] Third account signed in')
 print('')
-print('[4] Fourth Google Account Signed In')
+print('[4] Fourth account signed in')
 print('')
 choice = int(input())
-
 if choice == 1:
     webbrowser.open('https://classroom.google.com/u/0/h')
-
 if choice ==2:
     webbrowser.open('https://classroom.google.com/u/1/h')
-
 if choice ==3:
     webbrowser.open('https://classroom.google.com/u/2/h')
-
 if choice ==4:
     webbrowser.open('https://classroom.google.com/u/3/h')
